0637c41efd4b60256e6ac36cc478d2116fc5888d
Rhinno7 <rhinno.ferramentas@gmail.com>
2023-09-18 17:48:46 -0300

    Atualização 18.09.2023

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function ativarLetra(elemento){
     const arrTexto = elemento.innerHTML.split('');
     elemento.innerHTML = "";
+    arrTexto.array.forEach((letra,i) => {
+        setTimeout(()=>{
+            elemento.innerHTML +=letra;
+        })
+        
+    });
 }
 const titulo = document.querySelector('.digitando');
