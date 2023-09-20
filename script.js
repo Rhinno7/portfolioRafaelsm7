6697290665bef31108b9026f8e17fcf269a13344
Rhinno7 <rhinno.ferramentas@gmail.com>
2023-09-20 17:35:23 -0300

    ajustes e testes que não deu certo 20.09.2023

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const textos = ' Backend Developer';
 const textosDois = ' Web Developer';
-function ativarLetra(elemento){
+/*function ativarLetra(elemento){
     const arrTexto = elemento.innerHTML.split('');
     elemento.innerHTML = "";
     arrTexto.forEach((letra,i) => {
@@ -15,14 +15,39 @@ ativarLetra(titulo);
 
 /*FUNÇÃO PARA TESTES 19.09.2023*/
 function testes(elemento){
-    elemento.innerHTML="";
+    //elemento.innerHTML="";
     const txt = textos.split('');
     const txtDois = textosDois.split('');
     txt.forEach((letra,i)=>{
         setTimeout(()=>{
             elemento.innerHTML+=letra;
         },75*i);
+
     })
+    const xxx = elemento.innerHTML;
+    const xxy = xxx.split('')
+    xxy.forEach((letrass,ii)=>{
+        setTimeout(()=>{
+            const g = xxy.pop()
+            elemento.innerHTML+=letrass;
+        },75*ii);
+    })
+    //elemento.innerHTML=xxx
+    /*const complet = elemento.innerHTML
+    const numTextos = textos.length
+    const contador = 0
+    const xx = [complet]
+    while(contador==numTextos){
+        
+        setTimeout(()=>{
+            const gg = xx.pop()
+        })
+       
+        gg = ff
+        contador++
+
+    }*/
+    
 
 
     
